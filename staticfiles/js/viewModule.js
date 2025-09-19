3340aae7aa3883fee68afdb9d2e670c1320ad1df
CI5co22 <arnoldajcip225@gmail.com>
2025-09-19 13:09:34 -0600

    responsive viewModule

--- staticfiles/js/viewModule.js
+++ staticfiles/js/viewModule.js
@@ -6,32 +6,29 @@ let contenedor = document.querySelectorAll(".recetas")
 const viewModule = localStorage.getItem("view")
 console.log(viewModule)
 
+
 if(viewModule == "grid")
 {
-    grid.classList.remove("no-select");
-    pilas.classList.add("no-select")
-    recetas.forEach(element => {
-        element.classList.add("receta-grid")
-        element.style.marginRight = "10px"
-    });
-    contenedor.forEach(element => {
-        element.style.display = "flex"
-    });
+    Grid()
 }
 else
 {
-    pilas.classList.remove("no-select");
-    grid.classList.add("no-select")
-    recetas.forEach(element => {
-        element.classList.remove("receta-grid")
-    });
-    contenedor.forEach(element => {
-        element.style.display = ""
-    });
+    Pilas()
 }
 
 pilas.addEventListener("click", event =>
 {
+    Pilas();
+})
+
+grid.addEventListener("click", event =>
+{
+    Grid()
+}
+)
+
+function Pilas()
+{
     pilas.classList.remove("no-select");
     grid.classList.add("no-select")
     recetas.forEach(element => {
@@ -39,13 +36,14 @@ pilas.addEventListener("click", event =>
     });
     contenedor.forEach(element => {
         element.style.display = ""
+        element.style.flexWrap = "nowrap"
+        element.classList.remove("recetas-grid")
     });
-
+    
     localStorage.setItem("view",'pilas')
 }
-)
 
-grid.addEventListener("click", event =>
+function Grid()
 {
     grid.classList.remove("no-select");
     pilas.classList.add("no-select")
@@ -53,9 +51,10 @@ grid.addEventListener("click", event =>
         element.classList.add("receta-grid")
         element.style.marginRight = "10px"
     });
-    contenedor.forEach(element => {
-        element.style.display = "flex"
+       contenedor.forEach(e => {
+        e.style.display = "flex"
+        e.style.flexWrap = "wrap"
+        e.classList.add("recetas-grid")
     });
     localStorage.setItem("view",'grid')
 }
-)
