8ce46ca6b7c949fcd8ba685345208009cbc202bb
CI5co22 <arnoldajcip225@gmail.com>
2025-09-18 22:08:25 -0600

    cambio de colores y efecto en botones

--- miApp/static/js/actualizarActivo.js
+++ miApp/static/js/actualizarActivo.js
@@ -1,28 +1,29 @@
-elemento = document.getElementById('switchCheckDefault');
 
+    let elemento = document.getElementById('cbx-3');
 
-elemento.addEventListener("click", function() {
-    let id = this.getAttribute("data-id");
-    const token = this.getAttribute("data-token");
+    if (elemento) {
+        elemento.addEventListener("click", function() {
+            let id = this.getAttribute("data-id");
+            let token = this.getAttribute("data-token");
 
-    $.ajax({
-        url: `/receta/activar/`, 
-        type: "POST",
-        data: {
-            'id': id,
-            'csrfmiddlewaretoken': token
-        },
-        success: function(resp) {
-            if (resp.status === "ok") {
-               console.log('Se actualizo correctamente');
-            } else {
-                alert("Error al cambiar");
-            }
-        },
-        error: function(xhr, status, error) {
-            alert("Error en la solicitud");
-        }
-    });
-});
-
+            $.ajax({
+                url: `/receta/activar/`, 
+                type: "POST",
+                data: {
+                    'id': id,
+                    'csrfmiddlewaretoken': token
+                },
+                success: function(resp) {
+                    if (resp.status === "ok") {
+                        console.log('Se actualizó correctamente');
+                    } else {
+                        alert("Error al cambiar");
+                    }
+                },
+                error: function() {
+                    alert("Error en la solicitud");
+                }
+            });
+        });
+    }
 
